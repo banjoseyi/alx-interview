8c3fa7d4347556e4f4cfc31d2010a2ffb1f36a42
banjoseyi <banjoseyi2@gmail.com>
2023-07-18 02:12:13 -0700

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
 
-""" 
-script that reads stdin line by line and computes metrics
-"""
+
 import sys
 
-statusCode = ['200', '301', '400', '401', '403', '404', '405', '500']
-total_file_size = 0
-newObj = dict.fromkeys(statusCode, 0)
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def evaluation():
-    print("File size: {}".format(total_file_size))
-    for key, value in sorted(newObj.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
-            
-
-count = 0
 try:
     for line in sys.stdin:
-        line = line.split()
-        count += 1
-        try:
-            total_file_size += int(line[-1])
-            if line[-2] in statusCode:
-                newObj[line[-2]] += 1
-        except (IndexError, ValueError):
-            pass
-        
-        if count % 10 == 0:
-            evaluation()
-except KeyboardInterrupt:
-    evaluation()
-    raise
-else:
-    evaluation() 
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value)) 
