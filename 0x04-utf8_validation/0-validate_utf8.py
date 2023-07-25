56e97eed98211b735f5f7ef63f5559e263d6883c
banjoseyi <banjoseyi2@gmail.com>
2023-07-25 13:37:19 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-
-""" 
-Method that determines if a given data set represents
-a valid UTF-8 encoding.
+"""
+UTF-8 Validation
 """
 
+
 def validUTF8(data):
-    number_of_bytes = 0
-    
-    for x in data:
-        if number_of_bytes == 0:
-            if x >> 5 == 0b1110 or x >> 5 == 0b110:
-                number_of_bytes = 1
-            elif x >> 4 == 0b1110:
-                number_of_bytes = 2
-            elif x >> 3 == 0b11110:
-                number_of_bytes = 3
-        else:
-            if x >> 6 != 0b10 or x >> 7 == 0b1:
+    """
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
+    """
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
-            number_of_bytes -= 1
-    return number_of_bytes == 0
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
