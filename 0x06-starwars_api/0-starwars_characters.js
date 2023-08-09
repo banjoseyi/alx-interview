cd5d3ab310333902381086d19f56123d6e965cc4
banjoseyi <banjoseyi2@gmail.com>
2023-08-09 05:54:10 -0700

    corrected

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,52 @@
 
 const request = require('request');
 
-const movieURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
 
-request(movieURL, asyn function (error, res, body) {
-  const array = [];
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
 
-  if (error) {
-    console.log(error);
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
   } else {
-    const movie = JSON.parse(body);
-    for (let r = 0; r < movie.characters.lenth; r++) {
-      array.push(movieCharacter(movie.characters[r]));
+    console.error('Error: Got no Characters for some reason');
+  }
+};
+
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
     }
   }
+};
 
-  let cast = await Promise.all(array);
-
-  cast = cast.map((actor) => JSON.parse(actor).name);
-  cast.forEach((actor) => console.log(actor));
-});
-
-function movieCharacter (eachCharacter) {
-  return new Promise((resolve, reject) => {
-    request(eachCharacter, function (error, res, body) {
-      if (error) {
-        reject(error);
-      }
-      resolve(body);
-    });
-  });
-}
+getCharNames();
